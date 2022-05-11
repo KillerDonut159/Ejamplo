f0bc21783c08b2eac1557edc6f5749a76ca30eea
KillerDonut159 <eatvoltage04@gmail.com>
2022-05-11 14:06:33 -0400

    Pythons and ducks

---
+++ ducktest.py
@@ -0,0 +1,8 @@
+number = 1000
+
+fullduck = "{} ducks were eaten! Good amount!"
+
+if number > 100:
+    print(fullduck.format(number))
+else:
+    print("Not enough ducks were eaten :(")
---
+++ ganggang.py
@@ -0,0 +1,23 @@
+print("python gang yeeee")
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+duck = "Ducks"
+print(duck)
+
+duck2 = "should be eaten"
+print(duck, duck2)
+
+number = 20
+print(number, duck, duck2)
+
+def moreducks():
+    number = 50
+    print(number, duck, duck2)
+
+moreducks()
+
+fullduck = "{} Ducks should be eaten"
+print(fullduck.format(number))
+
